4a8b1546002796ee5d3e2a42a58065a4be120227
RainChan <mzdluo123@foxmail.com>
2021-09-18 21:22:08 +0800

    集成tf

--- app/src/main/java/win/rainchan/aishot/aishot/activity/CameraActivity.kt
+++ app/src/main/java/win/rainchan/aishot/aishot/activity/CameraActivity.kt
@@ -4,30 +4,77 @@ import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.tensorflow.lite.examples.poseestimation.camera.CameraSource
 import splitties.experimental.ExperimentalSplittiesApi
 import splitties.permissions.ensurePermission
 import splitties.toast.toast
 import win.rainchan.aishot.aishot.R
+import win.rainchan.aishot.aishot.ai.data.Device
+import win.rainchan.aishot.aishot.ai.ml.ModelType
+import win.rainchan.aishot.aishot.ai.ml.MoveNet
 import win.rainchan.aishot.aishot.databinding.ActivityCameraAvtivityBinding
 import java.lang.IllegalStateException
+
 
 class CameraActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCameraAvtivityBinding
+    private lateinit var cameraSource: CameraSource
+
     @ExperimentalSplittiesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraAvtivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        lifecycleScope.launch {
-            requirePermission()
+        lifecycleScope.launch(Dispatchers.Default) {
+
+            withContext(Dispatchers.Main) {
+                requirePermission()
+
+            }
+
         }
+
+
+    }
+
+
+    private suspend fun openCamera() {
+        cameraSource =
+            CameraSource(binding.frameCamera, object : CameraSource.CameraSourceListener {
+                override fun onFPSListener(fps: Int) {
+                    // todo fps
+                }
+
+                override fun onDetectedInfo(
+                    personScore: Float?,
+                    poseLabels: List<Pair<String, Float>>?
+                ) {
+                    // todo 结果
+
+                }
+
+            }).apply {
+                prepareCamera()
+            }
+        withContext(Dispatchers.Main) {
+            cameraSource.initCamera()
+        }
+        cameraSource.setDetector(
+            MoveNet.create(
+                this@CameraActivity,
+                Device.NNAPI,
+                ModelType.Thunder
+            )
+        )
+
     }
 
     @ExperimentalSplittiesApi
     private suspend fun requirePermission() {
-
         ensurePermission(
             permission = Manifest.permission.CAMERA,
             {
@@ -39,7 +86,24 @@ class CameraActivity : AppCompatActivity() {
             }, {
                 throw IllegalStateException("授权失败")
             })
+    }
 
+
+    override fun onResume() {
+        cameraSource.resume()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        cameraSource.close()
+        super.onPause()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        lifecycleScope.launch {
+            openCamera()
+        }
     }
 
 }
