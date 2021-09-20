470168b5e88d02522e83e006dd77e0ae1adeb88a
RainChan <mzdluo123@foxmail.com>
2021-09-20 22:16:46 +0800

    init parse

---
+++ app/src/main/java/win/rainchan/aishot/aishot/APP.kt
@@ -0,0 +1,17 @@
+package win.rainchan.aishot.aishot
+
+import android.app.Application
+import com.parse.Parse
+
+class APP : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Parse.initialize(
+            Parse.Configuration.Builder(this)
+                .applicationId(BuildConfig.APP_ID)
+                .clientKey(BuildConfig.APP_KEY)
+                .server(BuildConfig.API_URL)
+                .build()
+        )
+    }
+}
