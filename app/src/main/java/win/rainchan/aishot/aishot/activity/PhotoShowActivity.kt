1a4ed65c84aea3d129a4eb52b588908a50203304
RainChan <mzdluo123@foxmail.com>
2021-10-01 15:26:49 +0800

    disable compose

--- app/src/main/java/win/rainchan/aishot/aishot/activity/PhotoShowActivity.kt
+++ app/src/main/java/win/rainchan/aishot/aishot/activity/PhotoShowActivity.kt
@@ -2,12 +2,15 @@ package win.rainchan.aishot.aishot.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import win.rainchan.aishot.aishot.databinding.ActivityPhotoShowBinding
 
+private lateinit var binding: ActivityPhotoShowBinding
 
 class PhotoShowActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        binding = ActivityPhotoShowBinding.inflate(this.layoutInflater)
+        setContentView(binding.root)
     }
 }
 
--- app/src/main/java/win/rainchan/aishot/aishot/ui/compose/ComposeFragment.kt
+++ app/src/main/java/win/rainchan/aishot/aishot/ui/compose/ComposeFragment.kt
@@ -3,14 +3,6 @@ package win.rainchan.aishot.aishot.ui.compose
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Card
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 
 
@@ -20,19 +12,6 @@ class ComposeFragment:Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = ComposeView(requireContext()).apply {
-        setContent {
-
-        }
+    ) = null
     }
 
-  
-}
-
-@Preview
-@Composable
-fun mainView(){
-     Card(modifier = Modifier.padding(10.dp)) {
-        Text(text = "啦啦",modifier = Modifier.padding(horizontal = 10.dp))
-    }
-}
