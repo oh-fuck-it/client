d15d3fde8813f0fb99b180080dfedd1b9bfae1af
RainChan <mzdluo123@foxmail.com>
2021-10-05 19:12:34 +0800

    浮动按钮

--- app/src/main/java/win/rainchan/aishot/aishot/ui/home/HomeFragment.kt
+++ app/src/main/java/win/rainchan/aishot/aishot/ui/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package win.rainchan.aishot.aishot.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import win.rainchan.aishot.aishot.activity.CameraActivity
 import win.rainchan.aishot.aishot.databinding.FragmentHomeBinding
 import win.rainchan.aishot.aishot.ui.gallery.GalleryViewModel
 
@@ -30,9 +28,7 @@ class HomeFragment : Fragment() {
 //        binding.homeRecycle.adapter = PhotoAdapter(galleryViewModel.dataList)
         binding.homeRecycle.layoutManager = GridLayoutManager(context, 2)
 
-        binding.button.setOnClickListener {
-            startActivity(Intent(requireContext(), CameraActivity::class.java))
-        }
+
         return binding.root
     }
 
