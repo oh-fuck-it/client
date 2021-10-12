35e83110af242848dde91ae3a9f7428b3c6700cf
RainChan <mzdluo123@foxmail.com>
2021-10-12 20:37:21 +0800

    add home img

--- app/src/main/java/win/rainchan/aishot/aishot/ui/home/HomeFragment.kt
+++ app/src/main/java/win/rainchan/aishot/aishot/ui/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package win.rainchan.aishot.aishot.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import win.rainchan.aishot.aishot.activity.CameraActivity
 import win.rainchan.aishot.aishot.databinding.FragmentHomeBinding
 import win.rainchan.aishot.aishot.ui.gallery.GalleryViewModel
 import win.rainchan.aishot.aishot.ui.gallery.PhotoAdapter
@@ -31,9 +29,6 @@ class HomeFragment : Fragment() {
             if (it.isNotEmpty()) {
                 adapter.setNewData(it.toMutableList())
             }
-        }
-        binding.button.setOnClickListener {
-            startActivity(Intent(requireContext(), CameraActivity::class.java))
         }
         galleryViewModel.fetchData()
         binding.homeRecycle.adapter = adapter
