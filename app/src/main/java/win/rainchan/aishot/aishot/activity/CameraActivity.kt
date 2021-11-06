7ac00a47dcfb6cb9f3df7474afe1a9bd2f70fa0c
holk <yellowmac@sina.com>
2021-11-06 13:01:02 +0800

    new

--- app/src/main/java/win/rainchan/aishot/aishot/activity/CameraActivity.kt
+++ app/src/main/java/win/rainchan/aishot/aishot/activity/CameraActivity.kt
@@ -1,10 +1,15 @@
 package win.rainchan.aishot.aishot.activity
 
 import android.Manifest
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
@@ -20,6 +25,8 @@ import win.rainchan.aishot.aishot.ai.ml.ModelType
 import win.rainchan.aishot.aishot.ai.ml.MoveNet
 import win.rainchan.aishot.aishot.databinding.ActivityCameraAvtivityBinding
 import java.io.File
+import java.io.FileOutputStream
+import java.lang.Exception
 
 
 class CameraActivity : AppCompatActivity() {
@@ -33,7 +40,7 @@ class CameraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraAvtivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnShot.setOnClickListener {
+        binding.markBtn.setOnClickListener {
             lifecycleScope.launch(Dispatchers.IO) {
 
                 val photo = cameraSource?.shot() ?: return@launch
@@ -43,10 +50,9 @@ class CameraActivity : AppCompatActivity() {
                     photo.compress(Bitmap.CompressFormat.JPEG, 80, it)
                 }
 
-                val data =  synchronized(cameraSource!!.lock){
-                        cameraSource?.detector?.estimateSinglePose(photo)?.toArray() ?: return@launch
+                val data = synchronized(cameraSource!!.lock) {
+                    cameraSource?.detector?.estimateSinglePose(photo)?.toArray() ?: return@launch
                 }
-
 
                 if (predictImage != null) {
                     withContext(Dispatchers.Main) {
@@ -64,30 +70,58 @@ class CameraActivity : AppCompatActivity() {
                 }
                 startActivity(
                     Intent(baseContext, PhotoShowActivity::class.java).putExtra(
-                        "bundle",bundle
+                        "bundle", bundle
                     )
                 )
                 photo.recycle()
 
             }
         }
-        binding.markBtn.setOnClickListener {
+        binding.btnShot.setOnClickListener {
             lifecycleScope.launch(Dispatchers.IO) {
                 val photo = cameraSource?.shot() ?: return@launch
 //                 拍照存储
-                val tmpFile = File.createTempFile(System.currentTimeMillis().toString(), "png")
-                tmpFile.outputStream().use {
-                    photo.compress(Bitmap.CompressFormat.JPEG, 80, it)
-                }
+                saveImg(photo, System.currentTimeMillis().toString(), this@CameraActivity)
                 photo.recycle()
-                startActivity(Intent(baseContext, PhotoMarkActivity::class.java).apply {
-                    putExtra("bundle", Bundle().apply {
-                        putString("photo", tmpFile.absolutePath)
-                    })
-                })
             }
         }
         shutHint()
+    }
+
+
+    private fun saveImg(bitmap: Bitmap, name: String, context: Context): Boolean {
+        try {
+            var sdcardPath = System.getenv("EXTERNAL_STORAGE");      //获得sd卡路径
+            var dir = "$sdcardPath/ai_shot/";                    //图片保存的文件夹名
+            var file = File(dir);                                 //已File来构建
+            if (!file.exists()) {                                     //如果不存在  就mkdirs()创建此文件夹
+                file.mkdirs();
+            }
+            var mFile = File(dir + name);                        //将要保存的图片文件
+            if (mFile.exists()) {
+                Toast.makeText(context, "该图片已存在!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            var outputStream = FileOutputStream(mFile);     //构建输出流
+            bitmap.compress(
+                Bitmap.CompressFormat.JPEG,
+                100,
+                outputStream
+            );  //compress到输出outputStream
+            var uri = Uri.fromFile(mFile);                                  //获得图片的uri
+            context.sendBroadcast(
+                Intent(
+                    Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+                    uri
+                )
+            ); //发送广播通知更新图库，这样系统图库可以找到这张图片
+            return true;
+        } catch (e: Exception) {
+            Log.d(TAG, "saveImg: 111111111111111111111111111111111111111111111")
+            e.printStackTrace()
+        };
+        return false;
     }
 
     private fun shutHint() {
