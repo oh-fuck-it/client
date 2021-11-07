aaf538d987211615a6839d67d04de457e13bf1b4
farewell12345 <1149558764@qq.com>
2021-11-07 22:00:38 +0800

    ossbucket

---
+++ app/src/main/java/win/rainchan/aishot/aishot/spider/Bean/CosBucket.kt
@@ -0,0 +1,62 @@
+
+
+import com.qcloud.cos.exception.CosClientException
+import com.qcloud.cos.exception.CosServiceException
+import com.qcloud.cos.model.*
+import ink.umb.cdn.web.pic.config.CosClient
+import java.io.ByteArrayInputStream
+import java.io.File
+import java.net.URL
+import java.util.*
+
+
+class CosBucket {
+    private lateinit var bucketName: String
+    private lateinit var bucket:Bucket
+    private var listObjectRequest:ListObjectsRequest = ListObjectsRequest()
+    private var appId = "1304224524"
+
+    private fun initListObjRequest(){
+        listObjectRequest.bucketName = bucketName
+        listObjectRequest.prefix = "images/"
+        listObjectRequest.delimiter = "/"
+        listObjectRequest.maxKeys = 10
+    }
+    constructor(bucketName: String)  {
+        val createBucketRequest = CreateBucketRequest("$bucketName-$appId")
+        try {
+            bucket = CosClient.addBucket(createBucketRequest)!!
+        }catch(e:CosServiceException){
+            e.printStackTrace()
+        }catch(e: CosClientException){
+            e.printStackTrace()
+        }
+        initListObjRequest()
+    }
+    constructor(bucket:Bucket){
+        this.bucket = bucket
+        this.bucketName = bucket.name
+        initListObjRequest()
+    }
+    public fun putObjRequest(file:File): URL? {
+        val prefix: String = file.name.substring(file.name.lastIndexOf(".") + 1)
+        val key = "images/${UUID.randomUUID()}.$prefix"
+        val putObjRequest = PutObjectRequest(bucketName,key,file)
+        CosClient.client.putObject(putObjRequest)
+        return CosClient.client.getObjectUrl(bucketName,key)
+    }
+    fun getObjList(number: Int): MutableList<COSObjectSummary>? {
+        var objListener = CosClient.client.listObjects(listObjectRequest)
+        for (i in 0..number) {
+            if (objListener.isTruncated) break
+            listObjectRequest.marker = objListener.nextMarker
+        }
+        objListener = CosClient.client.listObjects(listObjectRequest)
+        return objListener.objectSummaries
+    }
+
+    fun deleteObjRequest(key:String){
+        CosClient.client.deleteObject(key,bucketName)
+    }
+
+}
---
+++ app/src/main/java/win/rainchan/aishot/aishot/until/CosClient.kt
@@ -0,0 +1,41 @@
+package ink.umb.cdn.web.pic.config;
+
+import CosBucket
+import com.qcloud.cos.COSClient
+import com.qcloud.cos.ClientConfig
+import com.qcloud.cos.auth.BasicCOSCredentials
+import com.qcloud.cos.model.Bucket
+import com.qcloud.cos.model.CreateBucketRequest
+import com.qcloud.cos.region.Region
+
+object CosClient {
+    private const val secretId = "AKIDQfeMnxrUqvkHXVDenfGadPu85k8F5eww"
+    private const val secretKey = "lFRM1PWMvDEyQcXBXdS0GGzRcW9VaPyG"
+    private val cred = BasicCOSCredentials(secretId,secretKey)
+    private val region = Region("ap-nanjing")
+    private val clientConfig = ClientConfig(region)
+    val client = COSClient(cred,clientConfig)
+    private var listBuckets = client.listBuckets()
+    fun getBucket(bucketName: String): CosBucket {
+        for (bucket in listBuckets){
+            if(bucket.name == bucketName){
+                return CosBucket(bucket)
+            }
+        }
+        throw NullPointerException("存储桶不存在")
+    }
+    fun getListBuckets():List<Bucket>{
+        return listBuckets
+    }
+    private fun flushBuckets(){
+        this.listBuckets = client.listBuckets()
+    }
+    fun addBucket(createBucketRequest: CreateBucketRequest): Bucket? {
+        val bucket = client.createBucket(createBucketRequest)
+        flushBuckets()
+        return bucket
+    }
+    fun deleteBucket(bucketName:String){
+        client.deleteBucket(bucketName)
+    }
+}
