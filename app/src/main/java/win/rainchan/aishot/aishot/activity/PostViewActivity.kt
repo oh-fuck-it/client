cf8ef00df3c455ff26a07ab88a6a56c3a9ff46ba
RainChan <mzdluo123@foxmail.com>
2021-11-09 12:16:44 +0800

    社区帖子假界面

---
+++ app/src/main/java/win/rainchan/aishot/aishot/activity/PostViewActivity.kt
@@ -0,0 +1,12 @@
+package win.rainchan.aishot.aishot.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import win.rainchan.aishot.aishot.R
+
+class PostViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_view)
+    }
+}
--- app/src/main/java/win/rainchan/aishot/aishot/ui/society/SocietyFragment.kt
+++ app/src/main/java/win/rainchan/aishot/aishot/ui/society/SocietyFragment.kt
@@ -1,13 +1,15 @@
 package win.rainchan.aishot.aishot.ui.society
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import win.rainchan.aishot.aishot.activity.PostViewActivity
 import win.rainchan.aishot.aishot.databinding.FragmentSocietyBinding
 
-class SocietyFragment : Fragment()  {
+class SocietyFragment : Fragment() {
     private lateinit var binding: FragmentSocietyBinding
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +17,9 @@ class SocietyFragment : Fragment()  {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSocietyBinding.inflate(inflater, container, false)
+        binding.fakeImg.setOnClickListener {
+            startActivity(Intent(requireContext(), PostViewActivity::class.java))
+        }
         return binding.root
     }
 
