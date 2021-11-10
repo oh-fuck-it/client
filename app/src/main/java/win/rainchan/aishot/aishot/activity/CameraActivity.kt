5fffc74ba1bf61eefc8eb953ab8e10942609a727
RainChan <mzdluo123@foxmail.com>
2021-11-10 12:07:47 +0800

    本地图片

--- app/src/main/java/win/rainchan/aishot/aishot/activity/CameraActivity.kt
+++ app/src/main/java/win/rainchan/aishot/aishot/activity/CameraActivity.kt
@@ -91,34 +91,6 @@ class CameraActivity : AppCompatActivity() {
         shutHint()
     }
 
-    // 获取当前目录下所有的mp4文件
-    private fun getFilesAllName(path: String?): MutableList<String> {
-        //传入指定文件夹的路径　　　　
-        var file = File(path);
-        val files: Array<File> = file.listFiles()
-        val imagePaths: MutableList<String> = ArrayList()
-        for (i in files.indices) {
-            if (checkIsImageFile(files[i].path)) {
-                imagePaths.add(files[i].path)
-            }
-        }
-        return imagePaths
-    }
-
-    /**
-     * 判断是否是照片
-     */
-    private fun checkIsImageFile(fName: String): Boolean {
-        var isImageFile = false
-        //获取拓展名
-        val fileEnd = fName.substring(
-            fName.lastIndexOf(".") + 1,
-            fName.length
-        ).lowercase(Locale.getDefault())
-        isImageFile =
-            fileEnd == "jpg" || fileEnd == "png" || fileEnd == "gif" || fileEnd == "jpeg" || fileEnd == "bmp"
-        return isImageFile
-    }
 
 
     @SuppressLint("SimpleDateFormat")
--- app/src/main/java/win/rainchan/aishot/aishot/activity/PhotoGallery.kt
+++ app/src/main/java/win/rainchan/aishot/aishot/activity/PhotoGallery.kt
@@ -1,9 +1,23 @@
 package win.rainchan.aishot.aishot.activity
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import win.rainchan.aishot.aishot.APP
+import win.rainchan.aishot.aishot.R
 import win.rainchan.aishot.aishot.databinding.ActivityPhotoGalleryBinding
 import win.rainchan.aishot.aishot.ui.gallery.GalleryViewModel
 import win.rainchan.aishot.aishot.ui.gallery.PhotoAdapter
@@ -11,7 +25,9 @@ import win.rainchan.aishot.aishot.ui.gallery.PhotoAdapter
 class PhotoGallery : AppCompatActivity() {
     @ExperimentalStdlibApi
     private val galleryViewModel: GalleryViewModel by viewModels()
+    private val localImgViewModel: LocalImgViewModel by viewModels()
     private var adapter = PhotoAdapter(arrayListOf())
+    private var localadapter = LocalPhotoAdapter(arrayListOf())
     private lateinit var binding: ActivityPhotoGalleryBinding
 
     @ExperimentalStdlibApi
@@ -30,12 +46,22 @@ class PhotoGallery : AppCompatActivity() {
             }
         }
 
+
+        localImgViewModel.imgList.observe(this) {
+            if (it.isNotEmpty()) {
+                // adapter.setDiffNewData(it.toMutableList())
+                localadapter.setNewInstance(it.toMutableList())
+            }
+        }
+
+
         galleryViewModel.fetchData()
+        localImgViewModel.loadData()
 
         binding.recy1.layoutManager = getLayoutManager()
         binding.recy2.layoutManager = getLayoutManager()
         binding.recy3.layoutManager = getLayoutManager()
-        binding.recy1.adapter = adapter
+        binding.recy1.adapter = localadapter
         binding.recy2.adapter = adapter
         binding.recy3.adapter = adapter
     }
@@ -45,3 +71,27 @@ class PhotoGallery : AppCompatActivity() {
     }
 }
 
+class LocalImgViewModel() : ViewModel() {
+    val imgList = MutableLiveData<List<String>>()
+
+    fun loadData() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val fileList = APP.ctx.getExternalFilesDir("data")?.listFiles()
+                ?.filter { it.name.endsWith(".jpg") }?.map { it.absolutePath } ?: return@launch
+            imgList.value = fileList
+        }
+    }
+}
+
+
+class LocalPhotoAdapter(data: MutableList<String>) :
+    BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_photo_list, data) {
+    @RequiresApi(Build.VERSION_CODES.R)
+    @SuppressLint("ResourceType")
+    override fun convert(holder: BaseViewHolder, item: String) {
+        val img = holder.getView<ImageView>(R.id.photo_view)
+        Glide.with(holder.itemView).load(item).into(img)
+
+    }
+
+}
